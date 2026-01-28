72de8d2c83b02c1a3635cea37c10a16071d5ff3b
kolaente <k@knt.li>
2026-01-28 13:46:23 +0100

    fix(octane): use SERVER_NAME as host for FrankenPHP HTTPS
    When HTTPS is enabled, use the SERVER_NAME environment variable as the
    host parameter instead of 0.0.0.0. This allows Caddy to properly identify
    the domain and obtain Let's Encrypt certificates.

--- octane/run-frankenphp.sh
+++ octane/run-frankenphp.sh
@@ -8,11 +8,13 @@ artisan-start /app/artisan
 
 HTTPS_FLAGS=""
 PORT=80
+HOST="0.0.0.0"
 case "$APP_URL" in
     https://*)
         HTTPS_FLAGS="--https --http-redirect"
         PORT=443
+        HOST="${SERVER_NAME:-localhost}"
         ;;
 esac
 
-exec php -d variables_order=EGPCS /app/artisan octane:start --server=frankenphp --host=0.0.0.0 --port=$PORT --admin-port 2019 $HTTPS_FLAGS
+exec php -d variables_order=EGPCS /app/artisan octane:start --server=frankenphp --host=$HOST --port=$PORT --admin-port 2019 $HTTPS_FLAGS
