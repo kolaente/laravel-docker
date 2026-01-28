cfbb1e88e64fe70920bbdfd1a8caf89d78b02c08
kolaente <k@knt.li>
2026-01-28 13:24:19 +0100

    fix(octane): use port 443 when HTTPS is enabled for FrankenPHP

--- octane/run-frankenphp.sh
+++ octane/run-frankenphp.sh
@@ -7,10 +7,12 @@ setup
 artisan-start /app/artisan
 
 HTTPS_FLAGS=""
+PORT=80
 case "$APP_URL" in
     https://*)
         HTTPS_FLAGS="--https --http-redirect"
+        PORT=443
         ;;
 esac
 
-exec php -d variables_order=EGPCS /app/artisan octane:start --server=frankenphp --host=0.0.0.0 --port=80 --admin-port 2019 $HTTPS_FLAGS
+exec php -d variables_order=EGPCS /app/artisan octane:start --server=frankenphp --host=0.0.0.0 --port=$PORT --admin-port 2019 $HTTPS_FLAGS
